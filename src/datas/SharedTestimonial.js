1e0af8f361f643bf5c5e2db1c07af23322326259
promzzy <promisestephen29@gmail.com>
2021-04-28 21:13:41 +0100

    created local API

---
+++ src/datas/SharedTestimonial.js
@@ -0,0 +1,89 @@
+export const Testimonial = [
+    {
+        id: "1",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: true
+    },
+    {
+        id: "2",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: false
+
+    },
+    {
+        id: "3",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: false
+
+    },
+    {
+        id: "4",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: false
+
+    },
+    {
+        id: "5",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: false
+
+    },
+    {
+        id: "6",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: false
+
+    },
+    {
+        id: "7",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: false
+
+    },
+    {
+        id: "8",
+        firstName: "promise",
+        lastName: "john",
+        image: "",
+        clientType: "vendor",
+        cityOfInstitution: "",
+        message: "",
+        hero: false
+
+    }
+]
